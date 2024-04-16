b1dcf790a20586e8c88a9f5188f1032296689c10
Kirill <pesterevkv2003@gmail.com>
2024-04-16 22:20:18 +0300

    add: start my project

---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheCard from "./TheCard.vue";
+
+const onClickAdd = () => {
+  alert("Добавить");
+};
+
+const onClickFavorite = () => {
+  alert("В закладки");
+};
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <TheCard
+      :price="5000"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :is-added="false"
+      :is-favorite="false"
+      :onClickAdd="onClickAdd"
+      :onClickFavorite="onClickFavorite"
+    />
+  </div>
+</template>
